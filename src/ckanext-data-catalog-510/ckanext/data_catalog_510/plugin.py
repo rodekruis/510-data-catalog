da5110b1dd30e189ae74db2b726f7bd4f1d770e9
Shubham Mahajan <shubham.mahajan@valuelabs.com>
2021-09-26 23:30:33 +0400

    [x][feature]: Add Country List to the Dataset

--- src/ckanext-data-catalog-510/ckanext/data_catalog_510/plugin.py
+++ src/ckanext-data-catalog-510/ckanext/data_catalog_510/plugin.py
@@ -2,11 +2,14 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckanext.data_catalog_510.\
      utils.validators import (validate_date_yyyy_mm_dd)
+from ckanext.data_catalog_510.\
+     utils.helpers import (get_countries)
 
 
 class DataCatalog510Plugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IValidators)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -19,4 +22,9 @@ class DataCatalog510Plugin(plugins.SingletonPlugin):
     def get_validators(self):
         return {
             'validate_date_yyyy_mm_dd': validate_date_yyyy_mm_dd
+        }
+
+    def get_helpers(self):
+        return {
+            'get_countries_list': get_countries
         }
