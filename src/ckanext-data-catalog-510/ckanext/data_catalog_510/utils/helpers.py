fc9aade4deec03bb13dd8549bf47bfafa7d10fc7
Shubham Mahajan <shubham.mahajan@valuelabs.com>
2021-11-02 17:49:31 +0400

    [m] Improvement in documentation and error handling

--- src/ckanext-data-catalog-510/ckanext/data_catalog_510/utils/helpers.py
+++ src/ckanext-data-catalog-510/ckanext/data_catalog_510/utils/helpers.py
@@ -10,6 +10,11 @@ log = logging.getLogger(__name__)
 
 
 def get_countries():
+    '''Helper used to fetch the country list by passing the limit as 400 as
+    currently the no of countries are less than that.
+    # TODO - Add a fallback if the API doesn't respond
+    :rtype: list - List of the countries
+    '''
     try:
         # URL for fetching countries
         url = 'https://goadmin.ifrc.org/api/v2/country/?limit=400&offset=0'
@@ -26,6 +31,15 @@ def get_countries():
 
 @core_helper
 def prefill_dataset_owner_details(data, call_type):
+    '''Helper used to prefill the dataset owner and data owner email on the
+    database basis of call_type
+    :param data: if the value is already present e.g. in case of edit
+    string.
+    :param call_type: to check if need to fetch name or email.
+    string().
+
+    :rtype: string
+    '''
     if data:
         return data
     else:
@@ -37,17 +51,36 @@ def prefill_dataset_owner_details(data, call_type):
 
 @core_helper
 def get_current_date(data):
-    format = "%Y-%m-%d"
-    if data:
-        return data
-    else:
-        return datetime.datetime.today().strftime(format)
+    '''Helper used to get the current date in the format 'yyyy-mm-dd'
+    :param data: if the value is already present e.g. in case of edit
+    string.
+
+    :rtype: string
+    '''
+    try:
+        format = "%Y-%m-%d"
+        if data:
+            return data
+        else:
+            return datetime.datetime.today().strftime(format)
+    except Exception as e:
+        log.error(e)
+        raise e
 
 
 @core_helper
 def get_storage_explorer_link(container):
-    subscription_id = config.get('ckan.azure_subscription_id')
-    datalake_account_name = config.get('ckan.datalake_account_name')
-    resource_group_name = config.get('ckan.azure_resource_group_name')
-    storage_link = f"storageexplorer://v=1&accountid=/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.Storage/storageAccounts/{datalake_account_name}&subscriptionid={subscription_id}&resourcetype=Azure.BlobContainer&resourcename={container}"
-    return storage_link
+    '''Helper used to return the azure storage explorer URL so that container
+    can be opened directly in storage explorer application.
+    :param container: The name of container which need to be opened 
+    string.
+
+    :rtype: string
+    '''
+    if container:
+        subscription_id = config.get('ckan.azure_subscription_id')
+        datalake_account_name = config.get('ckan.datalake_account_name')
+        resource_group_name = config.get('ckan.azure_resource_group_name')
+        storage_link = f"storageexplorer://v=1&accountid=/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.Storage/storageAccounts/{datalake_account_name}&subscriptionid={subscription_id}&resourcetype=Azure.BlobContainer&resourcename={container}"
+        return storage_link
+    return None
