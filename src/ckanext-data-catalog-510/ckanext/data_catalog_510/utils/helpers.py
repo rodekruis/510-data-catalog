0efb3fa66c15bb1ef07944435379bc6c5a54850c
Rahul Pavithran <rahul.pavithran@valuelabs.com>
2021-12-13 21:10:42 +0530

    Added url encoding for line breaks.

--- src/ckanext-data-catalog-510/ckanext/data_catalog_510/utils/helpers.py
+++ src/ckanext-data-catalog-510/ckanext/data_catalog_510/utils/helpers.py
@@ -153,7 +153,7 @@ def get_request_data_mailTo(package, res):
         ccEmail = email_template.get('cc').replace('.', '@@')
         resource_url = config.get('ckan_site_url') + '/dataset/' + package.get('dataset_name') + '/resource/' + res.get('id')
         subject = email_template.get('subject').format(res.get('name')).replace(" ", "%20")
-        body = email_template.get('body').format(res.get('name'), package.get('dataset_name'), resource_url).replace(" ", "%20")
+        body = email_template.get('body').format(res.get('name'), package.get('dataset_name'), resource_url).replace(" ", "%20").replace("\n", "%0A")
         return f'mailto:{toEmail}?cc={ccEmail}&subject={subject}&body={body}'
 
 @core_helper
