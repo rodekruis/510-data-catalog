3f4c9ed1db0eba2be2c6a94ee69fac85e4d5cd39
Rahul Pavithran <rahul.pavithran@valuelabs.com>
2021-12-16 14:44:42 +0530

    Added components for login.

--- src/ckanext-data-catalog-510/datapub-510-custom-ui/src/app/common/common.module.ts
+++ src/ckanext-data-catalog-510/datapub-510-custom-ui/src/app/common/common.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { commonComponents } from './components';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [...commonComponents],
+  declarations: [...commonComponents, LoginComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [FormsModule, ReactiveFormsModule, ...commonComponents],
 })
